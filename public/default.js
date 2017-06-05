d9b6f9fd2176cb5a9572f8e60bb476f9fd0d7b6e
Andrew L <alavaia@yahoo.com>
2017-06-05 08:55:50 -0400

    Refactor. Removed JGo and Chess dependencies

--- public/default.js
+++ public/default.js
@@ -5,23 +5,20 @@
       
       var socket, serverGame;
       var username, playerColor;
-      var game, board;
-
       var usersOnline = [];
       var myGames = [];
       socket = io();
 
       // WGo variables
-      var gogame;
+      var game;
       var gameSize = 19;
       var gameRepeat = "KO";  // options available: KO (ko is properly handled - new position cannot be same as previous position)
                               // ALL (same position cannot be repeated), NONE (positions can be repeated)
-      //var gogame = new WGo.Game(gameSize, gameRepeat);
-      var goboard = new WGo.Board(document.getElementById("GOboard"), {width: 600,});
+      var board = new WGo.Board(document.getElementById("board"), {width: 600,});
 
 
       // Hide go board until game is initiated
-      $('#GOboard').hide();
+      $('#board').hide();
 
            
       //////////////////////////////
@@ -54,41 +51,32 @@
 
           $('#page-lobby').show();
           $('#page-game').hide();
-          $('#GOboard').hide();
+          $('#board').hide();
         }            
       });
                   
       socket.on('joingame', function(msg) {
         console.log("joined as game id: " + msg.game.id );   
         playerColor = msg.color;
-        initGame(msg.game);
+        //initGame(msg.game); // start chess game
         initializeGame(msg.game);
         
         // !!!
         // if no prior moves in game then reset game and redraw board:
-        // gogame.firstPosition();
-        // drawBoard(gogame, goboard);
+        // game.firstPosition();
+        // drawBoard(game, board);
 
         $('#page-lobby').hide();
         $('#page-game').show();
-        $('#GOboard').show();
+        $('#board').show();
         
       });
-        
+
       socket.on('move', function (msg) {
         if (serverGame && msg.gameId === serverGame.id) {
-           game.move(msg.move);
-           board.position(game.fen());
-        }
-      });
-
-
-      socket.on('GOmove', function (msg) {
-        if (serverGame && msg.gameId === serverGame.id) {
-          console.log(msg.gogame);
-          //serverGame.game = msg.gogame;
-          move(gogame, msg.x, msg.y, gogame.turn); 
-          drawBoard(gogame, goboard); 
+          //console.log(msg.game);
+          move(game, msg.x, msg.y, game.turn); 
+          drawBoard(game, board); 
         } 
       });
     
@@ -119,7 +107,7 @@
         socket.emit('login', username);
 
         $('#page-game').hide();
-        $('#GOboard').hide();
+        $('#board').hide();
         $('#page-lobby').show();
       });
       
@@ -128,7 +116,7 @@
         
         socket.emit('login', username);
         $('#page-game').hide();
-        $('#GOboard').hide();
+        $('#board').hide();
         $('#page-lobby').show();
       });
       
@@ -174,7 +162,7 @@
       //////////////////////////////
       // Chess Game
       ////////////////////////////// 
-      
+/*      
       var initGame = function (serverGameState) {
         serverGame = serverGameState; 
         
@@ -229,109 +217,7 @@
         board.position(game.fen());
       };
 
-
-
-      //////////////////////////////
-      // JGo Game
-      ////////////////////////////// 
-
-/*
-      var updateCaptures = function (node) {
-        document.getElementById('black-captures').innerText = node.info.captures[JGO.BLACK];
-        document.getElementById('white-captures').innerText = node.info.captures[JGO.WHITE];
-      };
-      var jrecord = new JGO.Record(19);
-      var jboard = jrecord.jboard;
-      var jsetup = new JGO.Setup(jboard, JGO.BOARD.largeWalnut);
-      var player = JGO.WHITE; // next player
-      var ko = false, lastMove = false; // ko coordinate and last move coordinate
-      var lastHover = false, lastX = -1, lastY = -1; // hover helper vars
-
-      jboard.setType(JGO.util.getHandicapCoordinates(jboard.width, 2), JGO.BLACK);
-
-      jsetup.setOptions({stars: {points:5}});
-
-
-      jsetup.create('board', function(canvas) {
-
-        //serverGame = serverGameState;
-
-        canvas.addListener('click', function(coord, ev) {
-          var opponent = (player == JGO.BLACK) ? JGO.WHITE : JGO.BLACK;
-
-          if(ev.shiftKey) { // on shift do edit
-            if(jboard.getMark(coord) == JGO.MARK.NONE)
-              jboard.setMark(coord, JGO.MARK.SELECTED);
-            else
-              jboard.setMark(coord, JGO.MARK.NONE);
-
-            return;
-          }
-
-          // clear hover away - it'll be replaced or then it will be an illegal move
-          // in any case so no need to worry about putting it back afterwards
-          if(lastHover)
-            jboard.setType(new JGO.Coordinate(lastX, lastY), JGO.CLEAR);
-
-          lastHover = false;
-
-          var play = jboard.playMove(coord, player, ko);
-
-          if(play.success) {
-
-            // broadcast move to opponent
-            socket.emit('GOmove', {GOcoord: coord, GOplayer: player, GOko: ko, gameId: serverGame.id});
-
-            node = jrecord.createNode(true);
-            node.info.captures[player] += play.captures.length; // tally captures
-            node.setType(coord, player); // play stone
-            node.setType(play.captures, JGO.CLEAR); // clear opponent's stones
-
-            if(lastMove)
-              node.setMark(lastMove, JGO.MARK.NONE); // clear previous mark
-            if(ko)
-              node.setMark(ko, JGO.MARK.NONE); // clear previous ko mark
-
-            node.setMark(coord, JGO.MARK.CIRCLE); // mark move
-            lastMove = coord;
-
-            if(play.ko)
-              node.setMark(play.ko, JGO.MARK.CIRCLE); // mark ko, too
-            ko = play.ko;
-
-            player = opponent;
-            updateCaptures(node);
-
-          } else alert('Illegal move: ' + play.errorMsg);
-        });
-
-        canvas.addListener('mousemove', function(coord, ev) {
-          if(coord.i == -1 || coord.j == -1 || (coord.i == lastX && coord.j == lastY))
-            return;
-
-          if(lastHover) // clear previous hover if there was one
-            jboard.setType(new JGO.Coordinate(lastX, lastY), JGO.CLEAR);
-
-          lastX = coord.i;
-          lastY = coord.j;
-
-          if(jboard.getType(coord) == JGO.CLEAR && jboard.getMark(coord) == JGO.MARK.NONE) {
-            jboard.setType(coord, player == JGO.WHITE ? JGO.DIM_WHITE : JGO.DIM_BLACK);
-            lastHover = true;
-          } else
-            lastHover = false;
-        });
-
-        canvas.addListener('mouseout', function(ev) {
-          if(lastHover)
-            jboard.setType(new JGO.Coordinate(lastX, lastY), JGO.CLEAR);
-
-          lastHover = false;
-        });
-      }); // end jsetup.create
-
 */
-
       //////////////////////////////
       // WGo Game
       ////////////////////////////// 
@@ -390,46 +276,44 @@
       var initializeGame = function(serverGameState) {
 
         serverGame = serverGameState; 
-        console.log (serverGame);
+        //console.log (serverGame);
 
-        if (serverGame.gogame == null ) {
-          gogame = new WGo.Game(gameSize, gameRepeat);
+        if (serverGame.game == null ) {
+          game = new WGo.Game(gameSize, gameRepeat);
           console.log("creating new game");
         } else {
-          gogame = new WGo.Game(serverGame.gogame);
-          //gogame = WGo.Game.create(serverGame.gogame); 
+          game = new WGo.Game(serverGame.game);
+          //game = WGo.Game.create(serverGame.game); 
           console.log("resuming game")
         }
 
        
-        console.log(gogame);
-        console.log(gogame instanceof WGo.Game);
-        console.log(gogame.getPosition() instanceof WGo.Position);
+        //console.log(game);
 
         // draw board
-        drawBoard(gogame, goboard);
+        drawBoard(game, board);
       };  
 
       var lastHover = false;
       var lastX = -1; 
       var lastY = -1; 
 
-      goboard.addEventListener("click", function(x, y) {
+      board.addEventListener("click", function(x, y) {
 
         //!!! check if it's correct player's move
 
-        if( move(gogame, x, y, gogame.turn) == 1 ) { // legal move
+        if( move(game, x, y, game.turn) == 1 ) { // legal move
           
           // draw updated position
-          drawBoard(gogame, goboard);
+          drawBoard(game, board);
 
           // broadcast move to opponent
-          socket.emit('GOmove', { x: x, y: y, color: gogame.turn, gameId: serverGame.id, gogame: gogame });
+          socket.emit('move', { x: x, y: y, color: game.turn, gameId: serverGame.id, game: game });
         }
 
       });
 
-      goboard.addListener('mousemove', function(coord, ev) {
+      board.addListener('mousemove', function(coord, ev) {
         if(coord.i == -1 || coord.j == -1 || (coord.i == lastX && coord.j == lastY))
           return;
 
@@ -451,7 +335,7 @@
         */
       });
 
-      goboard.addListener('mouseout', function(ev) {
+      board.addListener('mouseout', function(ev) {
         if(lastHover) {
           jboard.setType(new JGO.Coordinate(lastX, lastY), JGO.CLEAR);
         }
